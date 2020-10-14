fca837b606be5b026f80418f3213bccd53ecb0ad
vic30004 <victor_993@msn.com>
2020-10-14 12:55:29 +0300

    started styling the login

--- gatsby-book-club/src/components/header.js
+++ gatsby-book-club/src/components/header.js
@@ -1,45 +1,83 @@
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useContext } from "react"
 import { FirebaseContext } from "./Firebase"
+import styled from "styled-components"
+
+const LogoutLink = styled.span`
+  cursor: pointer;
+  color: #fff;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+const HeaderWrapper = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const HeaderContent = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  display: flex;
+
+  > h1 {
+    margin: 0;
+    flex-grow: 1;
+    > a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+  > div {
+    margin: auto 0;
+  }
+`
+
+const UserInfo = styled.div`
+  text-align: right;
+  color: #ffff;
+`
+
+const LoginLink = styled.div`
+margin:auto 0;
+>a{
+  color:#ffff;
+}
+`
 
 const Header = ({ siteTitle }) => {
+  const { firebase, user } = useContext(FirebaseContext)
+  console.log(firebase, user)
+  const handleLogout = e => {
+    e.preventDefault()
+    firebase.logout().then(() => navigate("/login"))
+  }
   return (
-    <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <FirebaseContext.Consumer>
-        {props => {
-          console.log(props)
-          return <div />
-        }}
-      </FirebaseContext.Consumer>
-    </div>
-  </header>
-
+    <HeaderWrapper>
+      <HeaderContent>
+        <h1>
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+        <div>
+          {!!user && !!user.email && (
+            <div>
+              Hello, {user.email}
+              <UserInfo>
+                <LogoutLink onClick={handleLogout}>Logout</LogoutLink>
+              </UserInfo>
+            </div>
+          )}
+          {(!user || !user.email) && (
+            <LoginLink>
+              <Link to="/login">Login</Link>
+            </LoginLink>
+          )}
+        </div>
+      </HeaderContent>
+    </HeaderWrapper>
   )
-  
 }
 
 Header.propTypes = {
